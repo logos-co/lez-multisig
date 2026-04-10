0fc5e6e4a1bc1478d99e2d00962a1046dedf0fda
jimmy-claw <261145897+jimmy-claw@users.noreply.github.com>
2026-04-10 11:20:07 +0000

    fix: align nssa_core to v0.2.0-rc1 for SPEL compatibility

--- methods/guest/src/bin/multisig.rs
+++ methods/guest/src/bin/multisig.rs
@@ -1,23 +1,5 @@
 #![no_main]
 
-use nssa_core::program::{ProgramInput, ProgramOutput, read_nssa_inputs};
-use multisig_core::Instruction;
+use multisig_program::multisig_program;
 
-risc0_zkvm::guest::entry!(main);
-
-fn main() {
-    let (ProgramInput { pre_states, instruction }, instruction_words) =
-        read_nssa_inputs::<Instruction>();
-
-    let pre_states_clone = pre_states.clone();
-
-    let (post_states, chained_calls) = multisig_program::process(&pre_states, &instruction);
-
-    ProgramOutput::new(
-        instruction_words,
-        pre_states_clone,
-        post_states,
-    )
-    .with_chained_calls(chained_calls)
-    .write();
-}
+risc0_zkvm::guest::entry!(multisig_program::main);
